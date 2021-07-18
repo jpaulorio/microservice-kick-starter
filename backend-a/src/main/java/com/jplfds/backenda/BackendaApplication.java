aab1b26dfdc29fd697928694ce5305a3c5ac5bdd
JP Silva <jplfdsilva@gmail.com>
2021-07-17 21:44:06 -0400

    Fix folder added as submodule

---
+++ backend-a/src/main/java/com/jplfds/backenda/BackendaApplication.java
@@ -0,0 +1,13 @@
+package com.jplfds.backenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendaApplication.class, args);
+	}
+
+}
---
+++ backend-a/src/test/java/com/jplfds/backenda/BackendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jplfds.backenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
